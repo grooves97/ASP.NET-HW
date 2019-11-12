c6843b01134f6e06a37e93758ea1edf0fbe3ecf3
Aseka <grooves97@gmail.com>
2019-11-12 12:05:10 +0600

    Added method "All()" to download zip archive album

---
+++ WebApplicationHW3/WebApplicationHW3/WebApplicationHW3/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationHW3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(string id)
+        {
+            return File($@"C:\Users\ww\Documents\GitHub\ASP.NET-HW\WebApplicationHW3\WebApplicationHW3\WebApplicationHW3\audio\{id}.mp3", "audio/mpeg", $"{id}.mp3");
+        }
+
+        public ActionResult All()
+        {
+            return File($@"C:\Users\ww\Documents\GitHub\ASP.NET-HW\WebApplicationHW3\WebApplicationHW3\WebApplicationHW3\audio\All.rar", "application/zip", $"album.zip");
+        }
+    }
+}
